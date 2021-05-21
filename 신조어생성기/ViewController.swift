da65f97a321a829aefdf71325e47a07616e99b71
songmi oh <dhthd1060@gmail.com>
2021-05-21 13:11:17 +0900

    Add files via upload

---
+++ 신조어생성기/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  신조어생성기
+//
+//  Created by j on 2019/12/07.
+//  Copyright © 2019 Claire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
